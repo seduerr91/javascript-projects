454129efa5de56116b197e454e3e4b33ef129d64
Sebastian <duerr.sebastian@gmail.com>
2021-09-24 18:20:28 +0300

    js projects

---
+++ oo-js/app.js
@@ -0,0 +1,12 @@
+const bcrypt = require('bcrypt');
+const colors = require('colors');
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
+
+bcrypt.genSalt(saltRounds, function(err, salt) {
+  bcrypt.hash(myPlaintextPassword, salt, function(err, hash) {
+      // Store hash in your password DB.
+      console.log(hash.red);
+  });
+});
